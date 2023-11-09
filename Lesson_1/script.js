2f8d69792e2157ef8f1f0742988ba036e9a10d32
Alexandr Vorobyev <vorobyev.alexandr-2017@yandex.ru>
2023-11-09 15:31:47 +0300

    task first block

---
+++ Lesson_1/script.js
@@ -0,0 +1,145 @@
+//Урок 1
+
+// const numberOfFilm = prompt ("Сколько фильмов вы уже посмотрели?");
+
+// const personalMovieDB = {
+//     count: numberOfFilm,
+//     movies: {},
+//     actors: {},
+//     ganres: [],
+//     privat: false,
+// };
+
+// const a = prompt ("Один из последних просмотренных фильмов?", ''),
+//       b = prompt ("На сколько цените его?", ''),
+//       c = prompt ("Один из последних просмотренных фильмов?", ''),
+//       d = prompt ("На сколько цените его?", '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+
+
+
+
+//Урок 2
+
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error1');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error2');
+// } else if (num > 100) {
+//     console.log('Mnogo');
+// } else{
+//     console.log('ok!');
+// }
+
+// (num === 50) ? console.log('ok!') : console.log('Error2');
+
+// const num = 0;
+
+// switch (num) {
+//     case 49:
+//         console.log('False');
+//         break;
+//     case 100:
+//         console.log('false');
+//         break;
+//     case 50:
+//         console.log('True');
+//         break
+//     default:
+//         console.log('not this time');
+//         break;
+// }
+
+
+
+
+
+//Урок 3 Что выведет консоль?
+
+// console.log( NaN || 2 || undefined );
+
+// console.log( 1 && 2 && 3 && NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( !1 && '' || !3 );
+
+// console.log(!25 || null && true );
+
+// console.log( true || null || !3 || undefined || NaN);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 4 === 4 && 3 > 1 || 5);
+
+// const hamburger = NaN;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// console.log(hamburger && cola )
+
+
+
+
+
+// Урок 4: циклы
+
+// let num = 50;
+
+// while(num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num <= 55)
+
+// for(let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i)
+    
+// }
+
